a40c6a7678688c600388358d6ec57313f494f3c0
Derrak Richard <derrak@gmail.com>
2022-06-17 09:54:07 -0700

    add basePrice prototype method to js

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,20 @@ function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
 }
+Pizza.prototype.basePrice = function() {
+  let basePrice = 0;
+  if (this.size === "small"){
+    basePrice = 10;
+  }
+  else if (this.size === "meduim"){
+    basePrice = 15;
+  }
+  else if (this.size === "large"){
+    basePrice = 20;
+  }
+  console.log("Pizza's base price is:" + basePrice);
+  return basePrice;
+}
+
+const myPizza = new Pizza("large", ["anchovies", "pineapple"]);
+myPizza.basePrice();
