e39e1ed951460cac050df697c07ec56f6a98552a
Derrak Richard <derrak@gmail.com>
2022-06-17 16:45:07 -0700

    WIP end of day; stuck on pricing function

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@
 //OrderBook objects contain a single property: An empty object called orders. 
 //This is where we'll store entries in our OrderBook. 
 //Each entry will be a Order object.
-
 function Order() {
   this.order = {};
   this.currentId = 0;
@@ -10,8 +9,8 @@ function Order() {
 
 Order.prototype.addOrder = function(pizza){
   pizza.id = this.assignId();
-  pizza.price = this.priceCalc();
   this.order[pizza.id] = pizza;
+  pizza.price = this.priceCalc();  //<------this is where I'm stuck
 };
 
 Order.prototype.assignId = function () {
@@ -19,16 +18,21 @@ Order.prototype.assignId = function () {
   return this.currentId;
 };
 
-Order.prototype.priceCalc = function() {
-  let price = 0;
-  if (this.size === "small"){
-    price = 10;}
-  else if (this.size === "meduim"){
-    price = 15;}
-  else if (this.size === "large"){
-    price = 20;}
-  console.log("Pizza's base price is:" + price);
-  return price;
+Order.prototype.priceCalc = function(id) {
+  let size = order.findPizza(id).size;
+  if (size === "small"){
+    order.findPizza(id).price = 10;}
+  else if (size === "meduim"){
+    order.findPizza(id).price = 15;}
+  else if (size === "large"){
+    order.findPizza(id).price = 20;}
+}
+
+Order.prototype.findPizza = function(id) {
+  if (this.order[id] != undefined) {
+    return this.order[id];
+  }
+  return false;
 }
 
 // Business Logic for Pizza ---------
@@ -56,9 +60,10 @@ $(document).ready(function() {
     $('input[name="toppingsCheck"]:checked').each(function(){
       inputtedToppings.push($(this).val());
     });
-    
+  
     let newPizza = new Pizza(inputtedPizzaSize,inputtedToppings);
     order.addOrder(newPizza);
+    price = order.priceCalc(newPizza);
     console.log(order.pizza);
   });
 });
